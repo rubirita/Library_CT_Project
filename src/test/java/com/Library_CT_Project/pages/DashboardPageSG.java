52ba5005dc57dd1e5ff346eb9dc7204aab5ffd51
hp <alikhan14338@gmail.com>
2023-03-07 22:49:04 -0500

    sajjad US

--- src/test/java/com/Library_CT_Project/pages/DashboardPageSG.java
+++ src/test/java/com/Library_CT_Project/pages/DashboardPageSG.java
@@ -1,12 +1,31 @@
 package com.Library_CT_Project.pages;
 
+import com.Library_CT_Project.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-public class DashboardPageSG extends BasePage {
+public class DashboardPageSG{
 
-   @FindBy(xpath = "//span[.='Dashboard']")
+    public DashboardPageSG(){
+        PageFactory.initElements(Driver.getDriver(),this );
+    }
+
+    @FindBy(id="inputEmail")
+    public WebElement emailAddress;
+
+    @FindBy(id="inputPassword")
+    public WebElement password;
+
+    @FindBy( xpath = "//button[@type='submit']")
+    public WebElement submit;
+
+
+
+    @FindBy(xpath = "//span[.='Dashboard']")
     public WebElement Dashboard;
+
+
 
 
 }
