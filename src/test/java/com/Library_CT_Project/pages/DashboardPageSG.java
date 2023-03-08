d6659bc4bc92c6994f2ace6123568cac2c01197b
hp <alikhan14338@gmail.com>
2023-03-07 21:14:23 -0500

    sajjad US

---
+++ src/test/java/com/Library_CT_Project/pages/DashboardPageSG.java
@@ -0,0 +1,12 @@
+package com.Library_CT_Project.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class DashboardPageSG extends BasePage {
+
+   @FindBy(xpath = "//span[.='Dashboard']")
+    public WebElement Dashboard;
+
+
+}
